9d39feeddd2096b828fba0c288313796302a6929
Maciej Mandzak <maciej.mandzak@sondasports.com>
2021-06-10 13:07:20 +0200

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import Shop from './pages/Shop/Shop';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={Shop} />
