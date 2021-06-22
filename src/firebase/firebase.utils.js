23b66015bacc6c8fb87701061aa773297158c3fe
Maciej Mandzak <maciej.mandzak@sondasports.com>
2021-06-22 11:27:19 +0200

    firebase structure works

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,6 +17,27 @@ firebase.initializeApp(config);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exist) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log(
+        'An error occured during adding a new user to database',
+        error.message
+      );
+    }
+  }
+};
+
 const provider = new firebase.auth.GoogleAuthProvider();
 
 provider.setCustomParameters({ prompt: 'select_account' });
